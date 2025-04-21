f25c851e5c6b726324c2d89086b958fd8e33c14f
Ikhlas Al Khusaibi <ekhlasalkhusaibi@gmail.com>
2025-04-21 14:51:45 +0400

    Some modification

--- calc.py
+++ calc.py
@@ -3,26 +3,36 @@ num_2=int(input("Enter 2nd Number: "))
 operation=input("Select + or - or * or / or ** or ! (factorial) or absolute or | (OR) or & (AND) or ^ (XOR): ")
 result=0
 
-if operation=="+":
+if operation=="+":  #Addition
     result=num_1+num_2
-elif operation=="-":
+
+elif operation=="-":  #Subtraction
     result=num_1-num_2
-elif operation=="*":
+
+elif operation=="*":  #Multiplication
     result=num_1*num_2
-elif operation=="/":
+
+elif operation=="/":   #Division
     result=num_1/num_2
-elif operation=="**":
+
+elif operation=="**":  #Power
     result=num_1**num_2
-elif operation=="!":
+
+elif operation=="!":  #Factorial
     result=1
     for i in range(1,num_1+1):
         result=result*i
-elif operation=="absolute":
+
+elif operation=="absolute":  #Absolute
     result=abs(num_1)
-elif operation=="|":
+
+elif operation=="|":  #OR
     result=num_1|num_2
-elif operation=="&":
+
+elif operation=="&":  #AND
     result=num_1 & num_2
-elif operation=="^":
+
+elif operation=="^":  #XOR
     result=num_1 ^ num_2
+    
 print("Result is:", result)
