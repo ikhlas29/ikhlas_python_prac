7093230d63bb29ee709edb14146afe906c02b013
Ikhlas Al Khusaibi <ekhlasalkhusaibi@gmail.com>
2025-04-21 10:05:28 +0400

    Power is working

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1=int(input("Enter 1st Number: "))
 num_2=int(input("Enter 2nd Number: "))
-operation=input("Select + or - or * or /: ")
+operation=input("Select + or - or * or / or **: ")
 result=0
 
 if operation=="+":
@@ -11,4 +11,6 @@ elif operation=="*":
     result=num_1*num_2
 elif operation=="/":
     result=num_1/num_2
+elif operation=="**":
+    result=num_1**num_2
 print("Result is:", result)
