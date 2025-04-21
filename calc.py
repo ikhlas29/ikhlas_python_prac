0fcd25bc3ada4d771ab79c93ef9dd6bbf865a565
Ikhlas Al Khusaibi <ekhlasalkhusaibi@gmail.com>
2025-04-21 12:18:54 +0400

    Absolute is working

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1=int(input("Enter 1st Number: "))
 num_2=int(input("Enter 2nd Number: "))
-operation=input("Select + or - or * or / or ** or ! (factorial): ")
+operation=input("Select + or - or * or / or ** or ! (factorial) or absolute: ")
 result=0
 
 if operation=="+":
@@ -17,5 +17,7 @@ elif operation=="!":
     result=1
     for i in range(1,num_1+1):
         result=result*i
+elif operation=="absolute":
+    result=abs(num_1)
 
 print("Result is:", result)
